6d9e65d9c73862eb67cc89dfbe6524260fccdc08
Murodil <murodil@cybertekschool.com>
2024-10-24 15:01:47 -0400

    day 4 codes

--- src/day04/ArrayUniqueValues.java
+++ src/day04/ArrayUniqueValues.java
@@ -1,10 +1,9 @@
 package day04;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ArrayUniqueValues {
-    public static void main(StringCharactersFrequency[] args) {
+    public static void main(String[] args) {
         int[] nums = {1,2,2,1,4,5,4};
         Set<Integer> uniqueNums = new LinkedHashSet<>();
         for (Integer num : nums) {
@@ -12,5 +11,9 @@ public class ArrayUniqueValues {
         }
 
         System.out.println(uniqueNums);
+
+        List<Integer> numsList = new ArrayList<>(List.of(33,33,555,555,222,12,12,12,12,0,0));
+        Set<Integer> numsSet = new LinkedHashSet<>(numsList);
+        System.out.println("numsSet = " + numsSet);
     }
 }
--- src/tasks/Task09FindDuplicatesInArray.java
+++ src/tasks/Task09FindDuplicatesInArray.java
@@ -10,6 +10,40 @@ public class Task09FindDuplicatesInArray {
         //1, 5
         nums = findDuplicates(nums);
         System.out.println(Arrays.toString(nums));
+
+        int[] nums2 = {1,1,4,3,1,5,6,5,2, 2};
+        nums2 = findUnique(nums2);
+        System.out.println(Arrays.toString(nums2));
+    }
+
+    public static int[] findUnique(int[] nums) {
+        //track nums that we already checked
+        List<Integer> checked = new ArrayList<>();
+        //array to store values that we know has duplicates
+        int[] dupArr = new int[nums.length];
+        int index = 0;
+        for(int each : nums) {
+
+            //check if already counted for this number
+            if (checked.contains(each)) {
+                continue;
+            }
+
+            //loop through array and count number of matches
+            int count = 0;
+            for(int temp : nums) {
+                if (temp == each) {
+                    count++;
+                }
+            }
+            //if duplicate found then add to dupArray
+            if(count == 1) {
+                checked.add(each);
+                dupArr[index++] = each;
+            }
+        }
+        //return only duplicate values part.
+        return Arrays.copyOf(dupArr, checked.size());
     }
 
     public static int[] findDuplicates(int[] nums) {
